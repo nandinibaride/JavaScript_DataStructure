a25bbcbe2c4c348e8fe5001816777bdd871e2ffc
nandinibaride <nandinibaride1234@gmail.com>
2021-10-26 13:24:52 +0530

    Added: Array problem in javasceript

---
+++ FindRepeateNumber.js
@@ -0,0 +1,26 @@
+/*
+@purpose:find the digits that are repeated twice like 33, 77, etc and store them in an array.
+@authur: Nandini.B
+*/
+let repeatedDigitArray = new Array();
+
+function checkPalindrome(number) 
+{
+    let reverseNumber = number.toString().split("").reverse();
+    if (number == reverseNumber.join("")) 
+    {
+        return true;
+    }
+    else 
+    {
+        return false;
+    }
+}
+
+for(let index = 0 ; index <= 100 ; index++)
+{
+    if(index > 10 && checkPalindrome(index))
+        repeatedDigitArray.push(index);
+}
+
+console.log("The Number Array is : " + repeatedDigitArray);
