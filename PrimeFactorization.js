9ecd18b60cde134c8d1713d7459973004b8185ee
nandinibaride <nandinibaride1234@gmail.com>
2021-10-26 11:46:44 +0530

    Refactor: To find prime factor

---
+++ PrimeFactorization.js
@@ -0,0 +1,38 @@
+/*
+@purpose: To find Prime fators.
+@authour: Nandini.B
+*/
+const prompt = require('prompt-sync')();
+
+ let count = 0;
+ let isPrime = 0;
+ let result = [];
+ let number = prompt("Enter the number to print prime factors: ");
+ 
+ for ( i = 2; i <= number; i++ ) 
+ {
+
+     if ( number % i == 0) 
+     {
+         isPrime = 1;
+         
+         for ( j = 2; j <= i / 2; j++) 
+         {
+
+             if ( i % j == 0) 
+             {
+                 isPrime = 0;
+                 break;
+             }
+         }
+         if ( isPrime == 1 ) 
+         {
+             result [count] = i;
+             count++;
+         }
+     }
+ }
+ if ( isPrime == 0 || isPrime == 1 ) 
+ {
+     console.log(result);
+ }
